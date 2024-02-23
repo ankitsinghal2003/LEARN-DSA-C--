7461757f0e9227da8cca33422a3e76d449d57810
ankitsinghal2003 <singhalankit729@gmail.com>
2024-02-23 15:20:08 +0530

    my today code

--- ARRAYS/arrayclass1.cpp
+++ ARRAYS/arrayclass1.cpp
@@ -1,11 +1,26 @@
-bool linearSearch(int arr[], int size, int target) {
+# include<iostream>
+using namespace std;
 
-  for(int i=0; i<size; i++) {
-    if(arr[i] == target) {
-      //found
-      return true;
+// This program counts the number of zeroes and one's in an array. It uses a function called 'count'.
+void count(int  arr[], int size){
+  int zerocount=0;
+  int onecount=0;
+  for(int i=0;i<size;i++){
+    if(arr[i]==0){
+      zerocount++;
     }
-  }
-  //not found
-  return false;
+    if(arr[i]==1){
+      onecount++;
+    }
+
+} 
+cout<<"Number of zeros: " <<zerocount<<endl;
+cout<<"Number of ones : " <<onecount<<endl;
 }
+int main(){
+  int arr[7]={0,0,1,0,1,0,1};
+  int size=7;
+  count(arr,size);
+}
+
+
