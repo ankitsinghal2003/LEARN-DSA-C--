f4d44a79dead89c2926b2e3b616e12ff478ac81a
ankitsinghal2003 <singhalankit729@gmail.com>
2024-03-05 23:53:42 +0530

    my leetcode pratice

--- ARRAYS/arrayclass1.cpp
+++ ARRAYS/arrayclass1.cpp
@@ -373,3 +373,27 @@ using namespace std;
 
 //     return 0;
 // }
+
+//////////////////////roatated 
+//  int n = nums.size(); 
+//         int low = 0; 
+//         int high = n - 1; 
+//         while (low <= high) { // Perform binary search until lower bound is less than or equal to upper bound
+//             int mid = (low + high) / 2; // Calculate the middle index
+//             if (nums[mid] == target) // If the middle element is the target, return its index
+//                 return mid;
+//             if (nums[low] <= nums[mid]) { // If left half is sorted
+//                 if (nums[low] <= target && target <= nums[mid]) { // Check if target lies in the left half
+//                     high = mid - 1; // Adjust upper bound
+//                 } else {
+//                     low = mid + 1; // Adjust lower bound
+//                 }
+//             } else { // If right half is sorted
+//                 if (nums[mid] <= target && target <= nums[high]) { // Check if target lies in the right half
+//                     low = mid + 1; // Adjust lower bound
+//                 } else {
+//                     high = mid - 1; // Adjust upper bound
+//                 }
+//             }
+//         }
+//         return -1;
