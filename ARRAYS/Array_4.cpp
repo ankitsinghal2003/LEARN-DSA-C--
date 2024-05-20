de5d7ae6b03050425bbbf9181d9777ae909009fb
ankitsinghal2003 <singhalankit729@gmail.com>
2024-05-20 19:08:16 +0530

    leetcode

---
+++ ARRAYS/Array_4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+
+// //// Shift Negative Numbers in one side--->>
+
+// void sortOneSide(int arr[], int n){
+//     int index=0;
+//     int j=0;
+//     for( index=0; index<n; index++){
+//         if(arr[index]<0){
+//         swap(arr[index], arr[j]);
+//         j++;
+//     }
+//     }
+// }
+int main(){
+    int arr[]={-10,-20,30,-40,-50,60,70};
+    int n=7;
+    // sort(arr,arr+n);
+    
+//     sortOneSide(arr,n);
+for (int i=0; i<n; i++){
+    cout<<arr[i]<<" ";
+// }
+}
+}
+
